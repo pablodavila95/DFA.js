fce5538314956b676bb5ec3e13b3627e47ce7cd9
Pablo Dávila <a00513157@itesm.mx>
2019-11-23 17:37:06 -0600

    Added the html table creation and linted code

--- main.js
+++ main.js
@@ -12,12 +12,9 @@ getASize = () => {
 getDictionary = () => {
     const remDup = e => [...new Set(e)].sort().join("");
 
-    let dictionary = [];
     let dictionaryValue = remDup(document.getElementById('dictionary').value);
-    let chars = dictionaryValue.split('');
-
-    dictionary.push(chars);
-    return dictionary;
+    //let chars = dictionaryValue.split();
+    return Array.from(dictionaryValue);
 };
 
 clear_table = () => {
@@ -29,27 +26,53 @@ arrayOfQs = () => {
     let data = [];
     let length = getQs(); // user defined length
 
-    for(let i = 0; i < length; i++) {
+    for (let i = 0; i < length; i++) {
         data.push("q" + i);
     }
-    console.log(data);
     return data;
 };
 
 
-generate_table = () => {
-    clear_table();
+function createTable() {
+    let myQs = arrayOfQs();
 
     let title = document.getElementById("tableTitle");
     title.innerText = getDictionary().toString();
 
     let rn = getQs();
     let cn = getASize();
+
     for (let r = 0; r < rn; r++) {
         let x = document.getElementById('table').insertRow(r);
+
+        let rad = document.createElement("INPUT");
+        rad.setAttribute("type", "radio");
+        rad.setAttribute("id", "radio" + r);
+        let chk = document.createElement("INPUT");
+        chk.setAttribute("id", "check" + r);
+        chk.setAttribute("type", "checkbox");
+
+        x.insertCell(0).appendChild(rad);
+        x.insertCell(0).appendChild(chk);
+
+
         for (let c = 0; c < cn; c++) {
-            let y = x.insertCell(c);
-            y.innerHTML = "Row - " + r + " Column-" + c;
+            let element = document.createElement("select");
+
+            element.setAttribute("id", "s" + c);
+
+            for (let i = 0; i < myQs.length; i++) {
+                let opt = myQs[i];
+                let z = document.createElement("option");
+                z.textContent = opt;
+                z.id = opt;
+
+                element.appendChild(z);
+            }
+            x.insertCell(c).appendChild(element)
         }
     }
-};
+}
+
+
+
