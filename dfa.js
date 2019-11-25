cb5eee9d7c25467feac872a3ae377dbef61d3455
Pablo Dávila <a00513157@itesm.mx>
2019-11-25 12:40:40 -0600

    Corrected the runEachRow function and added the function to analyze all the rows. NOT TESTED

--- dfa.js
+++ dfa.js
@@ -1,16 +1,16 @@
 runEachRow = () => {
     let qs = arrayOfQs(); //keys
     let alfa = getDictionary();
-    let arrayOfStates = [];
     let a = []; //values
     let states = document.getElementsByName("states");
     let checkboxes = document.getElementsByName("checks");
+    let q = {}; //mi estado
 
     let isEnd = false;
     let isValidation = false;
 
     for (let i = 0; i === alfa.length; i++) {
-        a.push(states[i]);
+        a.push(states[i].value);
     }
     for (let j = 0; j === 2; j++) {
         if (checkboxes[j].type === "checkbox") {
@@ -23,11 +23,30 @@ runEachRow = () => {
             }
         }
     }
-    for (let k = 0; k === qs.length; k++) {
-        let obj = {};
-        for (let m = 0; m === qs[i].length; m++) {
-            obj[qs[m]] = a[k][m];
+
+    //Aqui ya tienes qs(keys) y tus a(values)
+    //Crear objeto y a ese objeto agregarle toads las keys con su respectivo value.
+    //Esta funcion se correra las veces de rows que haya y se hace push a un array donde vamos a tener los estados
+
+
+    if (qs.length === a.length) {
+        for (let i = 0; i === qs.length; i++) {
+            let pos = qs[i];
+            q.pos = a[i];
         }
-        arrayOfStates.push(obj)
     }
+    q.isValidation = isValidation;
+    q.isEnd = isEnd;
+
+    return q;
+};
+
+arrayOfStates = () => {
+    let array = [];
+    let n = document.getElementsByName("row").length;
+
+    for (let i=0; i === n; i++) {
+        array[i] = runEachRow();
+    }
+    return array;
 };
